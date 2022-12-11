9a35c880a1850c369515905a15b11cacdc30465c
Raghav Mangal <raghav.mangal@mfine.co>
2022-12-11 23:26:25 +0530

    Completed initial setup with database connection

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-console.log("Start")
+const express = require('express');
+const connectDB = require('./database/mongodb');
+const dotenv = require('dotenv');
+const app = express();
+
+//load environment variables
+dotenv.config();
+
+//connect database
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+
+//Testing route
+app.get('/', (req, res) => res.send('API Running'));
+
+//Define routes
+app.use('/api/order', require('./routes/order.route'));
+app.use('/api/service', require('./routes/service.route'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+
+module.exports = app;
