8f962f571179f16479b74863fee244e411b424e9
Harsh Rajput <hrrajput20@gmail.com>
2023-03-11 17:07:37 +0530

    add express router and db injected as a runtime dependency

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
-const sequelize = require("./util/database");
-const Order = require("./models/order");
+require('dotenv').config();
+const express = require('express')
 
-console.log("initialising...")
+//sequelize db dependecy injected at runtime to make it loose coupled. helps to provide testing or even mock db as well.
+module.exports = function(sequelize) {
+    console.log("initialising...")
+    const app = express()
 
-sequelize
-  .sync({force: true})
-  // .sync()
-  .then((result) => {
-    console.log('connection established to postgres');
-    // return Order.create({total: 1200021})
-  })
-//   .then(orders => {
-//     console.log("order created : ", orders.dataValues );
-//   })
-  .catch((err) => {
-    console.log('Error in connecting to Postgres db', err);
-  });
+    sequelize
+    .sync({force: true})
+    // .sync()
+    .then((result) => {
+        console.log('connection established to postgres');
+        // return Order.create({total: 1200021})
+    })
+    //   .then(orders => {
+        //     console.log("order created : ", orders.dataValues );
+    //   })
+    .catch((err) => {
+        console.log('Error in connecting to Postgres db', err);
+    });
+    
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
+    
+    app.get('/', (req, res) => {
+        console.log('GET / homepage request')
+        res.send('Hello World!')
+    })
 
-  module.exports = {};
+    return app
+}
--- server.js
+++ server.js
@@ -1,3 +1,9 @@
-require('./app')
+const db = require("./util/database");
+const makeApp = require('./app')
 
 console.log("server.js file")
+const port = process.env.SERVER_PORT || 8080
+
+const app = makeApp(db)
+
+app.listen(port, () => console.log("listening on port:", port))
