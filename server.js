6c48e0e27e801132453ad7cf4cf1f96fc858219b
Shashank Tyagi <Shashankcode99>
2023-04-17 23:31:47 +0530

    Shashank Tyagi Submission

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("Start")
+console.log("Start");
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const orderRoutes = require("./routes/orders.routes.js"); //orders routes
+
+app.use(express.json());
+app.use(morgan());
+
+app.use("/", orderRoutes);
+app.use("/add", orderRoutes);
+
+//Listening the app on PORT 3000
+app.listen(3000, () => {
+  console.log(`Backend Server is Running On : 3000`);
+});
