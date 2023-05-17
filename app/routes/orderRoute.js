b3afa6e721ab0e7ad704426a6aa237c062d13b46
maaz1996 <maaz.abdullah111@gmail.com>
2023-05-18 03:47:54 +0530

    order changes

--- app/routes/orderRoute.js
+++ app/routes/orderRoute.js
@@ -6,10 +6,10 @@ const orderController = require("../controllers/orderController");
 router.get("/orders/getall", middleware.auth, orderController.getAllOrders);
 router.get("/orders/get", middleware.auth, orderController.getOneOrder);
 
-router.get("/orders/getall", middleware.auth, orderController.getAllOrders);
+router.post("/orders/post", middleware.auth, orderController.getAllOrders);
 
-router.get("/orders/getall", middleware.auth, orderController.getAllOrders);
+router.put("/orders/update", middleware.auth, orderController.getAllOrders);
 
-router.get("/orders/getall", middleware.auth, orderController.getAllOrders);
+router.delete("/orders/delete", middleware.auth, orderController.getAllOrders);
 
 module.exports = router;
