86107d484a75ada2f6e032a6bf223acf381675d3
ritik619 <ritiksaini619@gmail.com>
2023-07-01 21:08:41 +0530

    Setup: Health Module and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Win Home Inspection OMS')
+    .setDescription('Order Management Service for Win home Inspection')
+    .setVersion('v1')
+    .addTag('oms')
+    .build();
+  app.useGlobalPipes(new ValidationPipe());
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.use(helmet());
+  app.enableCors();
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
