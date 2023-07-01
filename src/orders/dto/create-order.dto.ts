2d2eabe2b42cb26c6b834c0af7ec1d894953287e
ritik619 <ritiksaini619@gmail.com>
2023-07-01 20:05:27 +0530

    updated: entities and dtos

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,42 @@
-export class CreateOrderDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+  ArrayNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsUUID,
+  ValidateNested,
+} from 'class-validator';
+import { UUID_VERSION } from 'src/constants';
+
+export class Service {
+  @ApiProperty({
+    description: 'Service ID associated with the order.',
+    type: String,
+  })
+  @IsUUID(UUID_VERSION)
+  id: string;
+
+  @ApiProperty({
+    description: 'amount associated with the Service.',
+    type: Number,
+  })
+  @IsPositive()
+  @IsNumber()
+  amount: number;
+}
+
+export class CreateOrderDto {
+  @ApiProperty({
+    description: 'A list of service IDs associated with the order.',
+    type: [Service],
+  })
+  @ArrayNotEmpty()
+  @ValidateNested()
+  @Type(() => Service)
+  services: Service[];
+
+  @ApiProperty({ description: 'ID of the user placing order', type: String })
+  @IsUUID(UUID_VERSION)
+  user_id: string;
+}
--- src/orders/dto/update-order.dto.ts
+++ src/orders/dto/update-order.dto.ts
@@ -1,4 +1,22 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateOrderDto } from './create-order.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsUUID, ValidateNested } from 'class-validator';
+import { UUID_VERSION } from 'src/constants';
+import { Service } from './create-order.dto';
+import { Type } from 'class-transformer';
 
-export class UpdateOrderDto extends PartialType(CreateOrderDto) {}
+export class UpdateOrderDto {
+  @ApiProperty({
+    description: 'ID of the order to updated.',
+    type: String,
+  })
+  @IsUUID(UUID_VERSION)
+  order_id: string;
+
+  @ApiProperty({
+    description: 'A list of service IDs associated with the order.',
+    type: [Service],
+  })
+  @ValidateNested()
+  @Type(() => Service)
+  services: Service[];
+}
