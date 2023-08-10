0f6981c5c400a44c1c086042b7779794c1e2258a
suraj kumar jha <sjha@stratbeans.com>
2023-08-10 14:37:16 +0530

    initial setup

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log("Start")
+const express = require('express');
+const mongoose = require('mongoose');
+
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log('DB is connected !!');
+    app.listen(3000, () => {
+        console.log('Node App is LIVE !!');
+    })
+})
+.catch(err => {
+    console.log(err);
+});
+
+module.exports = app;
