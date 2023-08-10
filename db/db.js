9d5a1f456509b7045a87a20da004faadb5fd7648
Aniroodh Parashar <parasharaniroodh@gmail.com>
2023-08-10 18:37:00 +0530

    Update db.js
    Removed DB credentials to be replaced

--- db/db.js
+++ db/db.js
@@ -3,20 +3,12 @@ const mysql = require('mysql2')
 
 const dbConfig = {
     host:'localhost',
-    user:'root',
-    password:'aniroodh',
+    user:'USERNAME',
+    password:'PASSWORD',
     database:'win-backend'
 }
 
 const connection = mysql.createPool(dbConfig)
 
-// connection.connect((err) => {
-//     if(err){
-//         console.error('Error connecting to the DB')
-//         return
-//     }else{
-//         console.log('DB connected')
-//     }
-// })
 
 module.exports = connection.promise()
