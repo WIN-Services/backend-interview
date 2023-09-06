eeb80213199dc5217f1c3486adea59328c764d23
Sidhant Kumar <sidhantkumar@AdministorsMBP5.alphionsee.in>
2023-09-07 02:06:52 +0530

    modules added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Configs } from './config/config';
+import { RolesGuard } from './role/role-guard';
+import { OmsModule } from './oms/oms.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    MongooseModule.forRoot(Configs().databases.mongo_db.url, {
+      dbName: 'win_oms',
+    }),
+    OmsModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
 })
 export class AppModule {}
