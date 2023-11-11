8bcd80e70fbb1ba41b489a813bd0394c3b386206
BISAL RANJAN PADHAN <80083792+bishal2002@users.noreply.github.com>
2023-11-11 09:08:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,46 @@
-console.log("Start")
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const http = require("http");
+
+const server = http.createServer(app);
+
+//router paths
+const orderRouter = require("./src/router/order.routes");
+const serviceRouter = require("./src/router/service.routes");
+
+const  morgan = require("morgan");
+const { connectDB }  = require('./src/config/dbConnection')
+
+
+connectDB();
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(morgan("combined"));
+
+// Passport middleware
+app.use(cors({ origin: "*" }));
+
+
+//Routers
+app.use("/api/v1/order", orderRouter);
+app.use("/api/v1/service", serviceRouter);
+
+
+
+app.get("/health-check", (req, res) => {
+    res.send("Server is running...");
+});
+
+
+let PORT = 5000
+
+server.listen(PORT, () => {
+    console.log("---------------------------------");
+    console.log(`🚀 App is listening on ${PORT} 🚀`);
+    console.log("---------------------------------");
+});
+
+module.exports = { app, server };
