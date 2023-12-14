d427853d89467c632139922845b16f74909b9caf
Astha Singh <55753554+asthasingh999@users.noreply.github.com>
2023-12-14 10:04:06 +0530

    Create types.ts

---
+++ src/constant/types.ts
@@ -0,0 +1,14 @@
+export type orderDetail = {
+  id: string;
+  datetime: Date;
+  totalfee: number;
+  services: string[];
+};
+
+export type userArray = {
+  user_name: string;
+  password: string;
+  user_type: string;
+  email: string;
+};
+
