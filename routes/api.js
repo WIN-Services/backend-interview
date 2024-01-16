81618a27b7ed51eab7a7fa074452c63434073a29
MOHAMMAD FAISAL ALIM <fahad@appzoy.com>
2024-01-16 13:20:07 +0530

    service api

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { deleteOrders, getAllOrders, getOrderById, submitData, updateOrder } from '../functions/order.js';
+import { createServiceOrders, deleteServiceOrders, getAllServices, getServiceOrderById } from '../functions/servies.js';
 
 const Router = express.Router();
 Router.route('/getallorders').get(getAllOrders);
@@ -7,5 +8,10 @@ Router.route('/getdatabyid/:id').get(getOrderById);
 Router.route('/deletebyid').delete(deleteOrders);
 Router.route('/submitdata').post(submitData);
 Router.route('/updatedata').put(updateOrder);
+
+Router.route('/getallservices').get(getAllServices);
+Router.route('/getservicebyid/:id').get(getServiceOrderById);
+Router.route('/deleteserviceid').delete(deleteServiceOrders);
+Router.route('/submitservice').post(createServiceOrders);
 
 export default Router;
